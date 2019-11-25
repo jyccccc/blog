02969ecb36cb2e6d6f01bf8b489f9a822ff64a93
Jiyouc <45361851+Jiyouc@users.noreply.github.com>
2019-11-25 18:12:00 +0800

    init repo

---
+++ src/main/java/com/blog/tocloud/ToCloudApplication.java
@@ -0,0 +1,13 @@
+package com.blog.tocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/tocloud/ToCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.tocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
